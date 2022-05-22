b5221c025dd8ad56ccc18609fc7658a15b9881e9
Ekram70 <ekramullah70@gmail.com>
2022-05-22 23:13:50 +0600

    sticky navbar and why choose us added

--- script/app.js
+++ script/app.js
@@ -1,6 +1,7 @@
 const mobileMenu = document.querySelector(".mobile-menu");
 const menuIcon = document.querySelector(".menu-icon");
 const preloader = document.querySelector(".preloader");
+const navContainer = document.querySelector("#sticky");
 
 // for preloader
 window.addEventListener("load", () => {
@@ -15,4 +16,15 @@ menuIcon.addEventListener("click", () => {
     ? (icon.classList = "fa-solid fa-xmark")
     : (icon.classList = "fa-solid fa-bars");
   mobileMenu.classList.toggle("menu-toggle");
+});
+
+// sticky navbar
+let sticky = navContainer.clientHeight;
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset >= sticky) {
+    navContainer.classList.add("sticky");
+  } else {
+    navContainer.classList.remove("sticky");
+  }
+  console.log(sticky);
 });
