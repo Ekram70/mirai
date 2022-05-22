2d4862553e1e2779515f58c62607c9a86b9b3046
Ekram Ullah Dewan <ekramullah70@gmail.com>
2022-05-22 10:03:04 +0600

    preloader added

--- script/app.js
+++ script/app.js
@@ -1,6 +1,14 @@
 const mobileMenu = document.querySelector(".mobile-menu");
 const menuIcon = document.querySelector(".menu-icon");
+const preloader = document.querySelector(".preloader");
 
+// for preloader
+window.addEventListener("load", () => {
+  preloader.style.display = "none";
+  document.body.style.overflow = "auto";
+});
+
+// for menu icon toggling and showing mobile menu
 menuIcon.addEventListener("click", () => {
   const icon = menuIcon.querySelector("i");
   icon.classList == "fa-solid fa-bars"
